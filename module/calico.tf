90d2331549eee0964867414397ea166096c2d9a0
Hasakelum <Hasakelum@Hasakelums-MacBook-Pro.local>
2020-02-11 13:04:14 +0530

    adding calico enable variable

--- module/calico.tf
+++ module/calico.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "calico" {
   count = var.calico_enabled ? 1 : 0
 
-  provisioner "remote-exec" {
+  provisioner "local-exec" {
     working_dir = path.module
     command = <<EOS
        kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
